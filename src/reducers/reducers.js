5c1e9e4f60427b6d9f1ba126c655eef858d5694c
Mike Holzbach <maholzbach@gmail.com>
2018-03-13 16:20:15 -0400

    Expand reducer to handle initial state properly.

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,7 +1,38 @@
 import { combineReducers } from "redux";
 import { SELECT_MONTH, CURRENT_MONTH } from "../actions/actions.js";
 
-function SelectedMonth(state = 0, action) {
+let currentMonth = new Date().getUTCMonth();
+
+const initialState = currentMonth => {
+  switch (currentMonth) {
+    case 0:
+      return 31;
+    case 1:
+      return 28;
+    case 2:
+      return 31;
+    case 3:
+      return 30;
+    case 4:
+      return 31;
+    case 5:
+      return 30;
+    case 6:
+      return 31;
+    case 7:
+      return 31;
+    case 8:
+      return 30;
+    case 9:
+      return 31;
+    case 10:
+      return 30;
+    case 11:
+      return 31;
+  }
+};
+
+function SelectedMonth(state = initialState(currentMonth), action) {
   switch (action.type) {
     case CURRENT_MONTH:
       return action.days;
